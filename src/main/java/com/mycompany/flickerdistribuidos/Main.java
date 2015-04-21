e1f417bf7597e8a766f3a87c5080437e35b7e2d8
giiade <juliojlgon@gmail.com>
2015-04-21 23:20:04 +0200

    Inicio

--- src/main/java/com/mycompany/flickerdistribuidos/Main.java
+++ src/main/java/com/mycompany/flickerdistribuidos/Main.java
@@ -7,23 +7,36 @@ package com.mycompany.flickerdistribuidos;
 
 import com.flickr4java.flickr.Flickr;
 import com.flickr4java.flickr.REST;
-import com.flickr4java.flickr.RequestContext;
 import com.urjc.java.pruautorizacionesflickr.AutorizacionesFlickr;
+
 /**
  *
  * @author JulioLopez
  */
 public class Main {
+    // TODO: Reestructurar clase para elegir archivos y subirlos.
+    //se tendrá que actualizar cada 3 segundos para mostrar estado de subida
+    //LLamaremos a la clase de subida desde aquí.
+    public static void main(String[] args) {
 
-    AutorizacionesFlickr autorizacionesFlickr
-            = new AutorizacionesFlickr();
-    Flickr flickr
-            = new Flickr(autorizacionesFlickr.getApi_key(),
-                    autorizacionesFlickr.getSecret(),
-                    new REST());
-    RequestContext requestContext
-            = RequestContext.getRequestContext();
-    
-    requestContext.setAuth(autorizacionesFlickr.getAuth());
+        String userId;
+        String username;
+
+        AutorizacionesFlickr autorizacionesFlickr
+                = new AutorizacionesFlickr();
+        Flickr flickr
+                = new Flickr(autorizacionesFlickr.getApi_key(),
+                        autorizacionesFlickr.getSecret(),
+                        new REST());
+
+        userId = autorizacionesFlickr.getUserID();
+        username = autorizacionesFlickr.getAuth().getUser().getUsername();
+        System.out.println(userId + " . " + username);
+
+    }
+
+
+    // dir.list() -> recuperaremos todos los ficheros que componen una carpeta en un arraylist o lo que queramos
+    // Los contamos y los subiriamos de manera asincrona.
     
 }
