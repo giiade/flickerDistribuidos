0d405795a8e0ae740854a293c9f58a231f6413bf
giiade <juliojlgon@gmail.com>
2015-04-21 23:20:20 +0200

    Inicio

---
+++ src/main/java/com/mycompany/flickerdistribuidos/FlickrHelper/PhotoUploader.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.flickerdistribuidos.FlickrHelper;
+
+/**
+ *
+ * @author Julio
+ */
+public class PhotoUploader {
+    //Esta todo en el main ahora. Esto va a ser una clase auxiliar enccargada de la subida de fotos
+    //Crearemos un metodo para subir fotos y unicamente habrá que llamar al metodo desde Main
+    // TODO: Subir las fotos. https://github.com/callmeal/Flickr4Java/blob/master/Flickr4Java/src/examples/java/UploadPhoto.java
+    
+    
+    
+}
